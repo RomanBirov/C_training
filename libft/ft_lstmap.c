5695bc373b164110c40a39b3a39a328b88fc4b72
Roman BIROV <rbirov@e1r5p1.unit.ua>
2019-09-05 18:01:22 +0300

    upload my project libft

---
+++ libft/ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbirov <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/04 16:04:41 by rbirov            #+#    #+#             */
+/*   Updated: 2018/11/10 12:13:42 by rbirov           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
+{
+	t_list		*new;
+	t_list		*list;
+
+	if (!lst || !f)
+		return (0);
+	list = f(lst);
+	new = list;
+	while (lst->next)
+	{
+		lst = lst->next;
+		if (!(list->next = f(lst)))
+		{
+			free(list->next);
+			return (0);
+		}
+		list = list->next;
+	}
+	return (new);
+}
